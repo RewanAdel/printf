b60a2e932bafeb3e38102fafe4e2ad83e4039b18
AbdouElN <aabdoubelkacem@gmail.com>
2023-06-27 15:53:35 -0700

    printf

--- _putchar.c
+++ _putchar.c
@@ -9,5 +9,31 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static int i;
+	static har buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, BUF, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+	{
+		buf[i++] = c;
+	}
+	return(1);
+}
+/**
+ * _puts - print string
+ * @str: string to be printed
+ * Return: void
+ */
+int _puts(char *str)
+{
+	char *a = str;
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	return(*str - a);
 }
